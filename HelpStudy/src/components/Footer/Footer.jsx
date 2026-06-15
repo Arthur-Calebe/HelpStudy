78b14ce63d9f2ba421b0b4c3bb1c1fb5e506e670
CaueCalebe <arthurcarvalhodas@gmail.com>
2026-06-15 18:05:39 -0300

    criando módulos reutilizaveis

---
+++ HelpStudy/src/components/Footer/Footer.jsx
@@ -0,0 +1,13 @@
+import "./footer.css";
+
+export const Footer = () => {
+    return(
+        <>
+        <footer class="footer">
+            <p>Projeto desenvolvido para ajudar estudantes</p>
+            <p>desenvolvido por Arthur Calebe</p>
+        </footer>
+            
+        </>
+    );
+};
